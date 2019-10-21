dad9f0cfad055a0ed9d85b3c4a01122614014666
eye20 <eye20@amherst.edu>
2019-10-21 14:58:33 -0400

    adding some code

---
+++ scratch/somecode.R
@@ -0,0 +1,20 @@
+require(mosaic)
+directory <- "gutenberg"
+
+files <- list.files(directory)
+n <- length(files)
+
+index <- c(1:n)
+
+getTitle <- function(index) {
+  lines <- readLines(paste(directory, "/", files[index], sep=""))
+  return(toString(lines[3]))
+}
+
+poemlist <- character(length(index))
+
+for (i in 1:length(index)) {
+  poemlist[i] <- getTitle(i)
+}
+
+df <- data.frame(index, files, poemlist)
