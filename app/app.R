f918e759c81e3bda9e240ebb38777b015fd478a8
eye20 <eye20@amherst.edu>
2019-10-22 11:41:32 -0400

    changes

--- app/app.R
+++ app/app.R
@@ -30,7 +30,7 @@ server <- function(input, output) {
     input$pickpoem
     
     require(mosaic)
-    directory <- "../gutenberg"
+    directory <- "poems-processed"
     
     files <- list.files(directory)
     n <- length(files)
