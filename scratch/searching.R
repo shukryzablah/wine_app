9ce17adb21e54068ebdb9eeb7fa9aa348bd0b54a
Shukry Zablah <shukryzablah@gmail.com>
2019-10-22 12:03:48 -0400

    added search utility

---
+++ scratch/searching.R
@@ -0,0 +1,18 @@
+library(purrr)
+library(stringr)
+
+search_file_for_pattern <- function(file_name, pattern) {
+    raw <- readLines(paste0("../poems-processed/", file_name))
+    mask <- str_detect(raw, pattern)
+    hits <- tibble(file = file_name, matches = raw[mask])
+    return(hits)
+}
+
+search_files_for_pattern <- function(file_names, pattern) {
+    all_hits <- file_names %>%
+        map_dfr(search_file_for_pattern, pattern)
+    return(all_hits)
+}
+
+file_names <- fs::dir_ls("../poems-processed")
+search_files_for_pattern(file_names, "love")
